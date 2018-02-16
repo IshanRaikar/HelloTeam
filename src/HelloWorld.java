8a68eedd1e8b805a638059868d79dd3da8f1475b
Unknown <20harmet@elmbrookstudents.org>
2018-02-16 13:37:01 -0600

    :(

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,8 +6,11 @@ public static void main (String[] args) {
 	System.out.println("Sharada phadnis");
 	System.out.println("Riley Feng");
 	System.out.println("Thomas Harmeyer");
+<<<<<<< HEAD
 	System.out.println("The Broken Movie");
 	System.out.println("Harry Potter");
+=======
 	System.out.println("fav");
+>>>>>>> sharmov
 }
 }
